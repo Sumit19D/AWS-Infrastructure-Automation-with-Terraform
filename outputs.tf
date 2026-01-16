0476744d1fec9aeb6ae1dd313b325bf79f161525
Sumit19D <sumitdorugade1@gmail.com>
2026-01-16 17:28:09 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
   description = "The ID of the EC2 instance"
-  value       = aws_instance.example.id
+  value       = aws_instance.test.id
 }
  
 output "public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.example.public_ip
+  value       = aws_instance.test.public_ip
 }
 
 output "private_ip" {
   description = "The private IP address of the EC2 instance"
-  value       = aws_instance.example.private_ip
+  value       = aws_instance.test.private_ip
 }
